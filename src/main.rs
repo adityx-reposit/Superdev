f3a87fd198965736f82d3c900490d6a6e736154e
adityx-reposit <adityayadav2022@vitbhopal.ac.in>
2025-07-23 16:12:28 +0530

    macros done

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,87 @@
-use std::fmt::write;
 
 
 
-struct User{
-    name:String,
-    age:u32
+// #[derive(Debug)]
+// struct User{
+//     name:String,
+//     age:u32
+// }
+
+
+
+// fn main() {
+//     let u = User{
+//         name:String::from("Aditya"),
+//         age:21
+//     };
+
+
+//     println!("{:?}",u);
+// }
+
+// procedural macros 
+
+use std::fmt::Error;
+
+
+
+trait Serialize{
+    fn serialize(&self)->Vec<u8>;
+}
+
+trait Desirialize:Sized{
+    fn desirialize(v:Vec<u8>)-> Swap;
 }
 
 
-impl std::fmt::Display for User{
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-       write!(f,"{} {}",self.name,self.age)
+
+struct Swap{
+    qty_1:u32,
+    qty_2:u32,
+    qty_3:u32,
+}
+
+impl Serialize for Swap{
+    fn serialize(&self)->Vec<u8> {
+       let mut v = Vec::new();
+            v.extend(self.qty_1.to_be_bytes());
+            v.extend(self.qty_2.to_be_bytes());
+            v.extend(self.qty_3.to_be_bytes());
+          
+       return v;
+        
     }
 }
-fn main() {
-    let u = User{
-        name:String::from("Aditya"),
-        age:21
-    };
 
 
-    println!("{}",u);
+impl Desirialize for Swap {
+    fn desirialize(v:Vec<u8>)-> Swap {
+        let qty1=v[0..3];
+        let qty2=v[0..4];
+        let qty3=v[0..6];
+
+        
+        return Swap{
+            qty_1,
+            qty_2,
+            qty_3
+        }
+    }
 }
+
+
+fn main(){
+   let swap= Swap{
+       qty_1:5,
+       qty_2:7,
+       qty_3:3,
+   };
+
+   let function = swap.serialize();
+   println!("{:?}",function)
+    
+  
+}
+
+
 
